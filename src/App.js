089d5ba9bcfb1674305a3110c714052f11dc9653
Priyanshu Raj <priyanshurocky5@gmail.com>
2024-03-20 20:45:58 +0530

    created standard structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+
+  
   return <div className="container">
-    Countdown Timer
+    <h2>Countdown Timer</h2>
+    <label>Select Date and Time to Begin Countdown </label>
+    <input type="datetime-local"/>
+    <br/>
+    <br/>
+    <button>Start Timer</button>
+    <button>Cancel Timer</button>
+    
   </div>;
 }
 
